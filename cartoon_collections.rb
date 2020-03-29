7202f7e97c11253a4fe02dec08f5fae5952d0850
Kyle Bradley <kylepersonalmail@gmail.com>
2020-03-29 14:11:25 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,8 +19,12 @@ end
 def find_the_cheese(str_array)
    cheese_types = ["cheddar", "gouda", "camembert"]
    value_indexc = str_array.find_index("cheddar")
-   value_indexg = str_array.find_index("cheddar")
+   value_indexg = str_array.find_index("gouda")
+   value_indexca = str_array.find_index("camembert")
    
+   min()
+   
+  
    do |x|
      x == "cheddar" || "gouda" || "camembert"
      
